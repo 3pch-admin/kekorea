3e9c98c223c79f0528d8bbff9e952787f146e023
jhkim <jhkim@3pchain.co.kr>
2023-01-25 16:27:46 +0900

    no message

--- kekorea/src/e3ps/korea/history/controller/HistoryController.java
+++ kekorea/src/e3ps/korea/history/controller/HistoryController.java
@@ -1,16 +1,21 @@
 package e3ps.korea.history.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import e3ps.admin.commonCode.CommonCode;
 import e3ps.admin.commonCode.service.CommonCodeHelper;
+import e3ps.admin.sheetvariable.service.CategoryHelper;
 import e3ps.controller.BaseController;
 
 @Controller
@@ -23,5 +28,21 @@ public class HistoryController extends BaseController {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("/jsp/korea/history/history-list.jsp");
 		return model;
+	}
+	
+	@Description(value = "이력관리 리스트 목록 가져오는 함수")
+	@ResponseBody
+	@RequestMapping(value = "/list", method = RequestMethod.POST)
+	public Map<String, Object> list(@RequestBody Map<String, Object> params) throws Exception {
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			result = CategoryHelper.manager.list(params);
+			result.put("result", SUCCESS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("result", FAIL);
+			result.put("msg", e.toString());
+		}
+		return result;
 	}
 }
