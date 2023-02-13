f60a5cbdfada356fccd4d0e3a13780659f1a0031
jhkim <jhkim@3pchain.co.kr>
2023-02-13 22:51:15 +0900

    no message

--- kekorea/src/e3ps/org/controller/OrgController.java
+++ kekorea/src/e3ps/org/controller/OrgController.java
@@ -4,16 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.context.annotation.Description;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import e3ps.admin.commonCode.service.CommonCodeHelper;
 import e3ps.controller.BaseController;
 import e3ps.org.service.OrgHelper;
 
