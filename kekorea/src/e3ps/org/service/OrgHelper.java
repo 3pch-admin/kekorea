f1e9645b0669bd0ffb1249f8b98d2497a8329136
jhkim <jhkim@3pchain.co.kr>
2023-06-22 11:23:26 +0900

    no message

--- kekorea/src/e3ps/org/service/OrgHelper.java
+++ kekorea/src/e3ps/org/service/OrgHelper.java
@@ -436,6 +436,10 @@ public class OrgHelper {
 		QuerySpecUtils.toInnerJoin(query, People.class, WTUser.class, "wtUserReference.key.id",
 				WTAttributeNameIfc.ID_NAME, idx, idx_w);
 
+		if (query.getConditionCount() > 0) {
+			query.appendAnd();
+		}
+
 		if (!StringUtils.isNull(key)) {
 			query.appendOpenParen();
 			QuerySpecUtils.toLike(query, idx, People.class, People.NAME, key);
